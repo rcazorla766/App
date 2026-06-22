e25c887a93c2670585f921a2a42346b36d1eaf29
rcazorla766 <rcazrola766@gmail.com>
2026-06-22 11:16:46 +0200

    prediction endpoint implemented (check URL/docs and make an example manually to see the endpoint in action)

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 
 from app.api.routes.health import router as health_router
 from app.services.prediction_service import PredictionService
+from app.api.routes.prediction import router as prediction_router
 
 #Service initialization
 prediction_service = PredictionService()
+
 
 app = FastAPI(
     title="AI Model Governance API",
@@ -16,7 +18,9 @@ app = FastAPI(
 
 app.state.prediction_service = prediction_service
 
+#include the routers for health and prediction endpoints
 app.include_router(health_router)
+app.include_router(prediction_router)
 
 
 @app.get("/")
